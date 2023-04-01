f6e9459da3a6b7f79bd625566a572a28b4f665f5
vanessa garcia <venessag0908@gmail.com>
2023-03-31 20:27:50 -0600

    array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers,name){
+    function checkName(name2){
+        const nameBig = name.toUpperCase();
+        const nameBig2 = name2.toUpperCase();
+        if (nameBig2 == nameBig){
+            return name;
+        };
+    }
+    return drivers.filter(checkName);
+}
+
+function fuzzyMatch(drivers,name){
+    function checkName(name2){
+        if(name2.slice(0,2)==name.slice(0,2)){
+            return name;
+        }
+    }
+    return drivers.filter(checkName);
+}
+
+function matchName(drivers,nameString){
+    function checkName(name2){
+        if (name2.name == nameString){
+            return name2;
+        }
+    }
+    return drivers.filter(checkName);
+}
